f8d065843eb864a254c0dd2cd946d168bef099b7
NguyenHaii <167160632+NguyenHaii@users.noreply.github.com>
2024-12-20 11:39:33 +0700

    Add files via upload

---
+++ Bbsort/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số phần tử của mảng: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phần tử thứ " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Mảng trước khi sắp xếp:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Mảng sau khi sắp xếp:");
+        printArray(arr);
+
+        scanner.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
